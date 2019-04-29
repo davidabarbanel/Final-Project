07e3ba56ee04fd06dab88b30bc6d847ab7e7899d
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-04-29 13:42:42 -0400

    Added a canvas

--- FinalProject.js
+++ FinalProject.js
@@ -1,9 +1,12 @@
+var canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 var player1 = new Player(100,250);
 var player2 = new Player(600,250);
 var wDown = false;
 var sDown = false;
 var aDown = false;
 var dDown = false;
+
 
 function start(){
 	clear();
