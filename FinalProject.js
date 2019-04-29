e2cb60f1b64c4a7052405641fcb8f421deec3b17
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-04-29 13:24:24 -0400

    Updated HTML and JS

--- FinalProject.js
+++ FinalProject.js
@@ -1,6 +1,17 @@
-var defender =
-var forward =
+var player1 = new Player(100,250);
+var player2 = new Player(600,250);
 var wDown = false;
 var sDown = false;
 var aDown = false;
 var dDown = false;
+
+function start(){
+	clear();
+	renderBackground();
+	checkKeyboardStatus();
+	checkPlayersBounds();
+	checkPlayer1_player2Collision();
+	movePlayers();
+	renderPlayers();
+
+out.innerHTML = "Player Score: " + player1.score;
