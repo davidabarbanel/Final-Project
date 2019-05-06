f306d2d3aba5c41e110641f748f9b5dd1772ccf9
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-06 08:52:31 -0400

    Added an image to player1

--- FinalProject.js
+++ FinalProject.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
-var player1 = new Player(100,250);
+var player1 = document.getElementById("player1")
+new Player(100,250);
 var player2 = new Player(600,250);
 var wDown = false;
 var sDown = false;
@@ -19,3 +20,16 @@ function start() {
 }
 
 out.innerHTML = "Player Score: " + player1.score;
+
+//*this function was copied*
+function Player(x,y){
+	this.x = x;
+	this.y = y;
+	this.size = 20;
+	this.xVel = 0;
+	this.yVel = 0;
+	this.score = 0;
+	this.accel = 0.55;
+	this.decel = 0.55;
+	this.maxSpeed = 3;
+}
