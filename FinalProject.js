099f98c62a739d796942175812f3671050e963b9
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-07 09:02:59 -0400

    added movement

--- FinalProject.js
+++ FinalProject.js
@@ -1,8 +1,7 @@
 var canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 var player1 = document.getElementById("player1")
-var player1 = new Player(75,175);
-var player2 = new Player(600,250);
+var player2 = document.getElementById("player2")
 var wDown = false;
 var sDown = false;
 var aDown = false;
@@ -18,67 +17,65 @@ function start() {
 	movePlayers();
 	renderPlayers();
 }
+//copied//
 
-out.innerHTML = "Player Score: " + player1.score;
-
-//*this function was copied*
 var myGamePiece;
 
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGameArea.start();
+		myGamePiece = new component(30, 30, "red", 10, 120);
+		myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+		canvas : document.createElement("canvas"),
+		start : function() {
+				this.canvas.width = 480;
+				this.canvas.height = 270;
+				this.context = this.canvas.getContext("2d");
+				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+				this.interval = setInterval(updateGameArea, 20);
+		},
+		clear : function() {
+				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		}
 }
 
 function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.update = function() {
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
+		this.width = width;
+		this.height = height;
+		this.speedX = 0;
+		this.speedY = 0;
+		this.x = x;
+		this.y = y;
+		this.update = function() {
+				ctx = myGameArea.context;
+				ctx.fillStyle = color;
+				ctx.fillRect(this.x, this.y, this.width, this.height);
+		}
+		this.newPos = function() {
+				this.x += this.speedX;
+				this.y += this.speedY;
+		}
 }
 
 function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
+		myGameArea.clear();
+		myGamePiece.newPos();
+		myGamePiece.update();
 }
 
 function moveup() {
-    myGamePiece.speedY -= 1;
+		myGamePiece.speedY -= 1;
 }
 
 function movedown() {
-    myGamePiece.speedY += 1;
+		myGamePiece.speedY += 1;
 }
 
 function moveleft() {
-    myGamePiece.speedX -= 1;
+		myGamePiece.speedX -= 1;
 }
 
 function moveright() {
-    myGamePiece.speedX += 1;
+		myGamePiece.speedX += 1;
 }
