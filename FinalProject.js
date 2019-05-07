8e3d8694f439660ca00ca5622b05bc3147d55353
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-07 09:09:14 -0400

    added growth to canvas

--- FinalProject.js
+++ FinalProject.js
@@ -29,8 +29,8 @@ function startGame() {
 var myGameArea = {
 		canvas : document.createElement("canvas"),
 		start : function() {
-				this.canvas.width = 480;
-				this.canvas.height = 270;
+				this.canvas.width = 1000;
+				this.canvas.height = 1000;
 				this.context = this.canvas.getContext("2d");
 				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 				this.interval = setInterval(updateGameArea, 20);
