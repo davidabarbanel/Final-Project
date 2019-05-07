0c31a350ff0d998e91184ab9f5726c976ccca32c
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-07 08:34:31 -0400

    added movement

--- FinalProject.js
+++ FinalProject.js
@@ -22,14 +22,63 @@ function start() {
 out.innerHTML = "Player Score: " + player1.score;
 
 //*this function was copied*
-function Player(x,y){
-	this.x = x;
-	this.y = y;
-	this.size = 20;
-	this.xVel = 0;
-	this.yVel = 0;
-	this.score = 0;
-	this.accel = 0.55;
-	this.decel = 0.55;
-	this.maxSpeed = 3;
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
+
+function moveup() {
+    myGamePiece.speedY -= 1;
+}
+
+function movedown() {
+    myGamePiece.speedY += 1;
+}
+
+function moveleft() {
+    myGamePiece.speedX -= 1;
+}
+
+function moveright() {
+    myGamePiece.speedX += 1;
 }
