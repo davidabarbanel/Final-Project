8d9900f44828373702344ead840c80b1ba0a46ba
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-16 08:35:06 -0400

    Update FinalProject.js

--- FinalProject.js
+++ FinalProject.js
@@ -10,6 +10,22 @@ function startGame() {
 		myGameArea.start();
 }
 
+
+//creating canvas
+var myGameArea = {
+		canvas : document.createElement("canvas"),
+		start : function() {
+				this.canvas.width = 800;
+				this.canvas.height = 500;
+				this.context = this.canvas.getContext("2d");
+				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+				this.interval = setInterval(updateGameArea, 20);
+		},
+		clear : function() {
+				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		}
+}
+
 //velocity
 function component(width, height, color, x, y) {
 		this.width = width;
@@ -52,19 +68,4 @@ function moveleft() {
 
 function moveright() {
 		myGamePiece.speedX += 1;
-}
-
-//creating canvas
-var myGameArea = {
-		canvas : document.createElement("canvas"),
-		start : function() {
-				this.canvas.width = 800;
-				this.canvas.height = 500;
-				this.context = this.canvas.getContext("2d");
-				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-				this.interval = setInterval(updateGameArea, 20);
-		},
-		clear : function() {
-				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		}
 }
