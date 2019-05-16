06b589a4c6dedac4884325c97d649f6c30f2a7f7
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-16 08:39:30 -0400

    Update FinalProject.js

--- FinalProject.js
+++ FinalProject.js
@@ -4,28 +4,6 @@ var player2 = document.getElementById("player2")
 
 var myGamePiece;
 
-//this part is creating the yellow block
-function startGame() {
-    myGamePiece = new component(60, 60, "yellow", 5, 100);
-		myGameArea.start();
-}
-
-
-//creating canvas
-var myGameArea = {
-		canvas : document.createElement("canvas"),
-		start : function() {
-				this.canvas.width = 800;
-				this.canvas.height = 500;
-				this.context = this.canvas.getContext("2d");
-				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-				this.interval = setInterval(updateGameArea, 20);
-		},
-		clear : function() {
-				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-		}
-}
-
 //velocity
 function component(width, height, color, x, y) {
 		this.width = width;
@@ -68,4 +46,27 @@ function moveleft() {
 
 function moveright() {
 		myGamePiece.speedX += 1;
+}
+
+
+//this part is creating the yellow block
+function startGame() {
+    myGamePiece = new component(60, 60, "yellow", 5, 100);
+		myGameArea.start();
+}
+
+
+//creating canvas
+var myGameArea = {
+		canvas : document.createElement("canvas"),
+		start : function() {
+				this.canvas.width = 800;
+				this.canvas.height = 500;
+				this.context = this.canvas.getContext("2d");
+				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+				this.interval = setInterval(updateGameArea, 20);
+		},
+		clear : function() {
+				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		}
 }
