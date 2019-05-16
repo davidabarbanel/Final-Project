7470fc17aa5df6dc00b17976c31105e1944ffc7c
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-16 08:58:42 -0400

    Update FinalProject.js

--- FinalProject.js
+++ FinalProject.js
@@ -8,7 +8,6 @@ var myGamePiece;
 //this part is creating the yellow block
 function startGame() {
     myGamePiece = new yellowBlock(60, 60, "yellow", 5, 100);
-    myGamePiece = new redBlock(20, 20, "red", 5, 50)
 		myGameArea.start();
 }
 
@@ -42,22 +41,6 @@ function yellowBlock(width, height, color, x, y) {
 				ctx.fillStyle = color;
 				ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
-
-//characteristics of Red block
-function redBlock(width, height, color, x, y) {
-		this.width = width;
-		this.height = height;
-		this.speedX = 0;
-		this.speedY = 0;
-		this.x = x;
-		this.y = y;
-		this.update = function() {
-				ctx = myGameArea.context;
-				ctx.fillStyle = color;
-				ctx.fillRect(this.x, this.y, this.width, this.height);
-		}
-
-
 
 
 
