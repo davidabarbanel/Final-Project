54566648a4b5e66b604ec30ff9e955751ee974c6
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-21 08:52:08 -0400

    Update FinalProject.js

--- FinalProject.js
+++ FinalProject.js
@@ -4,7 +4,7 @@ var myGamePiece;
 
 //this part is creating the blocks when the game starts
 function startGame() {
-    myGamePiece = new yellowBlock(50, 50, "yellow", 5, 70);
+    myGamePiece = new yellowBlock(50, 50, "yellow", 5, 120);
     myGameArea.start();
 }
 
@@ -19,7 +19,6 @@ var myGameArea = {
 				this.context = this.canvas.getContext("2d");
 				document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 				this.interval = setInterval(updateGameArea, 20);
-        myGameArea = new redBlock(20, 20, "red", 5, 50);
 		},
 		clear : function() {
 				this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
