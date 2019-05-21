60f84a597391800b3f2fdc49a06592280a773b7a
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-21 09:05:35 -0400

    Update FinalProject.js

--- FinalProject.js
+++ FinalProject.js
@@ -52,6 +52,11 @@ function redBlock(width, height, color, x, y) {
   this.height = height;
   this.x = x;
   this.y = y;
+  this.update = function() {
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 }
 
 
