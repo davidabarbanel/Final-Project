eb27fe103a9f90ad82f66fc0f042b5006dcd8fd1
davidabarbanel <david.abarbanel20@alumni.loyola.ca>
2019-05-27 12:58:28 -0400

    Update FinalProject.js

--- FinalProject.js
+++ FinalProject.js
@@ -15,8 +15,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 600;
-        this.canvas.height = 350;
+        this.canvas.width = 800;
+        this.canvas.height = 400;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
